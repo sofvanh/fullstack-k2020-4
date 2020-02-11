4c206f18ec79d5abe017aeb30fffac4427ac947f
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-11 15:11:16 +0200

    favoriteBlog and tests (4.5)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,19 @@
 const totalLikes = (blogs) => {
-    return(blogs.reduce((total, blog) => total + blog.likes, 0))
+    return (blogs.reduce((total, blog) => total + blog.likes, 0))
+}
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null
+    return (blogs.reduce((oldFave, blog) => {
+        if (oldFave === null) return (blog)
+        else {
+            if (blog.likes > oldFave.likes) return blog
+            else return oldFave
+        }
+    }))
 }
 
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
