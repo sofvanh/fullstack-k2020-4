83898acf507880be77880b62e58da7dfe97c2611
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-11 11:08:34 +0200

    config.js and blog.js

--- app.js
+++ app.js
@@ -3,18 +3,10 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const app = require('./app')
 const http = require('http')
+const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-const PORT = 3003
+const PORT = config.PORT
 server.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`)
 })
