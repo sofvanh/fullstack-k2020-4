298643dc48eb39f266fa43f337cc51d1c6f6e843
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-11 10:42:47 +0200

    Requests to test functionality (4.1)

--- index.js
+++ index.js
@@ -6,39 +6,39 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(cors())
 app.use(express.json())
 
 app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+    Blog
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
 })
 
 app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+    const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
 })
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
