81d779e54afa8c3795814831e93c47fba4b33c46
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-13 18:45:50 +0200

    bcrypt, user model, and adding users

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -20,6 +21,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+    const body = request.body
+    const passwordHash = await bcrypt.hash(body.password, 10)
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+    response.json(savedUser)
+})
+
+module.exports = usersRouter
