6ea36eb30c32ac8a81a4e5dda0387d4310c80f46
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-15 12:38:32 +0200

    Logging in & token (4.18)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -23,6 +24,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
