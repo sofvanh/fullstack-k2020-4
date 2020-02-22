93c54dc9a077091b37db09ef76ccafa97033308f
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-22 12:58:58 +0200

    Fixes and formatting to tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -6,128 +6,115 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 const initialBlogs = [
-    {
-        _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
-        author: "Michael Chan",
-        url: "https://reactpatterns.com/",
-        likes: 7,
-        user: "5e457ce03521ea6cbe0be68a",
-        __v: 0
-    },
-    {
-        _id: "5a422a851b54a927234d17f7",
-        title: "FASHION 101 FOR NOOBS",
-        author: "Linus",
-        url: "https://fashion.com/",
-        likes: 200,
-        user: "5e457ce03521ea6cbe0be68a",
-        __v: 0
-    }
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    user: "5e457ce03521ea6cbe0be68a",
+    __v: 0
+  },
+  {
+    _id: "5a422a851b54a927234d17f7",
+    title: "FASHION 101 FOR NOOBS",
+    author: "Linus",
+    url: "https://fashion.com/",
+    likes: 200,
+    user: "5e457ce03521ea6cbe0be68a",
+    __v: 0
+  }
 ]
 
-const testUser = {
-    _id: "5e457ce03521ea6cbe0be68a",
-    username: "violet",
-    name: "Violet Gray",
-    passwordHash: "$2b$10$/9MHQN0bScx/zNos3h9Lj.To0uj.aZYjqikEGhFlXA66.AVZl7VMK",
-    __v: 0,
-    blogs: [
-        "5a422a851b54a676234d17f7"
-    ]
-}
-
 beforeEach(async () => {
-    await Blog.deleteMany({})
-    await Blog.insertMany(initialBlogs)
-    await User.deleteMany({})
-    await User.insertMany([testUser])
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogs)
 })
 
 describe('GET / call', () => {
-    test('returns json', async () => {
-        await api
-            .get('/api/blogs')
-            .expect(200)
-            .expect('Content-Type', /application\/json/)
-    })
+  test('returns json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-    test('returns two blogs', async () => {
-        const response = await api.get('/api/blogs')
-        expect(response.body.length).toBe(2)
-    })
+  test('returns two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(2)
+  })
 
-    test('returns blogs with ids instead of _ids', async () => {
-        const response = await api.get('/api/blogs')
-        expect(response.body[0].id).toBeDefined
-    })
+  test('returns blogs with ids instead of _ids', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined
+  })
 })
 
 describe('POST / call', () => {
-    test('adds a blog', async () => {
-        const newBlog = {
-            title: "Being Awesome",
-            author: "Michelle Obama",
-            url: "https://awesome.com/",
-            likes: 100
-        }
+  test('adds a blog', async () => {
+    const newBlog = {
+      title: "Being Awesome",
+      author: "Michelle Obama",
+      url: "https://awesome.com/",
+      likes: 100
+    }
 
-        await api
-            .post('/api/blogs')
-            .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
-            .send(newBlog)
+    await api
+      .post('/api/blogs')
+      .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
+      .send(newBlog)
 
-        const blogs = await api.get('/api/blogs')
-        expect(blogs.body[2].title).toBe(newBlog.title)
-    })
+    const blogs = await api.get('/api/blogs')
+    expect(blogs.body[2].title).toBe(newBlog.title)
+  })
 
-    test('sets likes to 0 by default', async () => {
-        const newBlog = {
-            title: "Basic Food for Basic Noobs",
-            author: "Gordon Ramsay",
-            url: "https://www.noobseat.com/"
-        }
+  test('sets likes to 0 by default', async () => {
+    const newBlog = {
+      title: "Basic Food for Basic Noobs",
+      author: "Gordon Ramsay",
+      url: "https://www.noobseat.com/"
+    }
 
-        const result = await api
-                    .post('/api/blogs')
-                    .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
-                    .send(newBlog)
-        expect(result.body.likes).toBe(0)
-    })
+    const result = await api
+      .post('/api/blogs')
+      .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
+      .send(newBlog)
+    expect(result.body.likes).toBe(0)
+  })
 
-    test('requires title', async () => {
-        const newBlog = {
-            author: "Jane Austen",
-            url: "https://www.realistic-life-in-england.com/"
-        }
+  test('requires title', async () => {
+    const newBlog = {
+      author: "Jane Austen",
+      url: "https://www.realistic-life-in-england.com/"
+    }
 
-        await api
-            .post('/api/blogs')
-            .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
-            .send(newBlog).expect(400)
-    })
+    await api
+      .post('/api/blogs')
+      .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
+      .send(newBlog).expect(400)
+  })
 
-    test('requires url', async () => {
-        const newBlog = {
-            title: "How to find a husband",
-            author: "Jane Austen"
-        }
+  test('requires url', async () => {
+    const newBlog = {
+      title: "How to find a husband",
+      author: "Jane Austen"
+    }
 
-        await api
-            .post('/api/blogs')
-            .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
-            .send(newBlog).expect(400)
-    })
+    await api
+      .post('/api/blogs')
+      .set('Authorization', 'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InZpb2xldCIsImlkIjoiNWU0NTdjZTAzNTIxZWE2Y2JlMGJlNjhhIiwiaWF0IjoxNTgxNzY3ODQ2fQ.Mbn0zpB9IOcnlLK5piqsvbEfN6WrGgXeTscBDKUhj7w')
+      .send(newBlog).expect(400)
+  })
 })
 
 describe('DELETE /:id call', () => {
-    test('does not delete without token', async () => {
-        const response = await api.delete('/api/blogs/5a422a851b54a676234d17f7')
-        expect(response.status).toBe(401)
-        expect(response.body.error).toBe('invalid token')
-    })
+  test('does not delete without token', async () => {
+    const response = await api.delete('/api/blogs/5a422a851b54a676234d17f7')
+    expect(response.status).toBe(401)
+    expect(response.body.error).toBe('invalid token')
+  })
 })
 
 afterAll(() => {
-    mongoose.connection.close()
+  mongoose.connection.close()
 })
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -5,80 +5,91 @@ const api = supertest(app)
 const User = require('../models/user')
 
 const initialUsers = [
-    {
-        _id: "5e457e129162ea6f49a784af",
-        username: "lentsikka",
-        name: "Lennu",
-        passwordHash: "$2b$10$7/A55bnVnAeIKvH6BcViIulI5McSxqKyGflNbXroUjMA714.NgEhm",
-        __v: 0
-    }
+  {
+    _id: "5e457e129162ea6f49a784af",
+    username: "lentsikka",
+    name: "Lennu",
+    passwordHash: "$2b$10$7/A55bnVnAeIKvH6BcViIulI5McSxqKyGflNbXroUjMA714.NgEhm",
+    __v: 0
+  },
+  {
+    _id: "5e457ce03521ea6cbe0be68a",
+    username: "violet",
+    name: "Violet Gray",
+    passwordHash: "$2b$10$/9MHQN0bScx/zNos3h9Lj.To0uj.aZYjqikEGhFlXA66.AVZl7VMK",
+    __v: 0,
+    blogs: [
+      "5a422a851b54a676234d17f7",
+      "5a422a851b54a927234d17f7"
+    ]
+  }
 ]
 
 beforeEach(async () => {
-    await User.deleteMany({})
-    await User.insertMany(initialUsers)
+  await User.deleteMany({})
+  await User.insertMany(initialUsers)
 })
 
 describe('POST / call', () => {
-    test('does not add short usernames', async () => {
-        const newUser = {
-            username: "ye",
-            name: "Yes man",
-            password: "noooo"
-        }
+  test('does not add short usernames', async () => {
+    const newUser = {
+      username: "ye",
+      name: "Yes man",
+      password: "noooo"
+    }
 
-        const response = await api.post('/api/users').send(newUser)
-        expect(response.status).toBe(400)
-        expect(response.body.error).toBe('User validation failed: username: Path `username` (`ye`) is shorter than the minimum allowed length (3).')
-    })
+    const response = await api.post('/api/users').send(newUser)
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('User validation failed: username: Path `username` (`ye`) is shorter than the minimum allowed length (3).')
+  })
 
-    test('requires username', async () => {
-        const newUser = {
-            name: "Yes man",
-            password: "noooo"
-        }
+  test('requires username', async () => {
+    const newUser = {
+      name: "Yes man",
+      password: "noooo"
+    }
 
-        const response = await api.post('/api/users').send(newUser)
-        expect(response.status).toBe(400)
-        expect(response.body.error).toBe('User validation failed: username: Path `username` is required.')
-    })
+    const response = await api.post('/api/users').send(newUser)
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('User validation failed: username: Path `username` is required.')
+  })
 
-    test('requires unique username', async () => {
-        const newUser = {
-            username: "lentsikka",
-            name: "Yes man",
-            password: "noooo"
-        }
+  test('requires unique username', async () => {
+    const newUser = {
+      username: "lentsikka",
+      name: "Yes man",
+      password: "noooo"
+    }
 
-        const response = await api.post('/api/users').send(newUser)
-        expect(response.status).toBe(400)
-        expect(response.body.error).toBe('E11000 duplicate key error collection: test.users index: username_1 dup key: { username: \"lentsikka\" }')
-    })
+    const response = await api.post('/api/users').send(newUser)
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('E11000 duplicate key error collection: test.users index: username_1 dup key: { username: \"lentsikka\" }')
+  })
 
-    test('does not add short passwords', async () => {
-        const newUser = {
-            username: "yes",
-            name: "Yes man",
-            password: "no"
-        }
+  test('does not add short passwords', async () => {
+    const newUser = {
+      username: "yes",
+      name: "Yes man",
+      password: "no"
+    }
 
-        const response = await api.post('/api/users').send(newUser)
-        expect(response.status).toBe(400)
-        expect(response.body.error).toBe('password not long enough')
-    })
+    const response = await api.post('/api/users').send(newUser)
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('password not long enough')
+  })
 
-    test('requires password', async () => {
-        const newUser = {
-            username: "yes",
-            name: "Yes man"
-        }
+  test('requires password', async () => {
+    const newUser = {
+      username: "yes",
+      name: "Yes man"
+    }
 
-        const response = await api.post('/api/users').send(newUser)
-        expect(response.status).toBe(400)
-        expect(response.body.error).toBe('password not long enough')
-    })
+    const response = await api.post('/api/users').send(newUser)
+    expect(response.status).toBe(400)
+    expect(response.body.error).toBe('password not long enough')
+  })
 })
 
 afterAll(() => {
-    mongoose.connection.close()
+  mongoose.connection.close()
 })
