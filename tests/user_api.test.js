93aba3fc8fb11b7382b4414ae99eb153a0bf4b3f
sofivanhanen <sofia.vanhanen0@gmail.com>
2020-02-22 15:52:57 +0200

    Fix user test

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -4,6 +4,25 @@ const app = require('../app')
 const api = supertest(app)
 const User = require('../models/user')
 
+const initialUsers = [
+  {
+    _id: "5e51117cfba7b553c3393e88",
+    username: "violet",
+    name: "Violet Gray",
+    passwordHash: "$2b$10$sCB5Gx8b1yjIup3kOfrh1e/JNj2wQK5chpCwAVIHynT3CYNMLX18O",
+    __v: 0,
+    blogs: [
+      "5a422a851b54a676234d17f7",
+      "5a422a851b54a927234d17f7"
+    ]
+  }
+]
+
+beforeEach(async () => {
+  await User.deleteMany({})
+  await User.insertMany(initialUsers)
+})
+
 describe('POST / call', () => {
   test('does not add short usernames', async () => {
     const newUser = {
@@ -30,14 +49,14 @@ describe('POST / call', () => {
 
   test('requires unique username', async () => {
     const newUser = {
-      username: "lentsikka",
+      username: "violet",
       name: "Yes man",
       password: "noooo"
     }
 
     const response = await api.post('/api/users').send(newUser)
     expect(response.status).toBe(400)
-    expect(response.body.error).toBe('E11000 duplicate key error collection: test.users index: username_1 dup key: { username: \"lentsikka\" }')
+    expect(response.body.error).toBe('E11000 duplicate key error collection: test.users index: username_1 dup key: { username: \"violet\" }')
   })
 
   test('does not add short passwords', async () => {
